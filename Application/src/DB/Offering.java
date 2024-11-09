3c29b97a8ca84a11cd4b72bc42769fc2091732fb
Hrag Bankian <hrag.bankian@gmail.com>
2024-11-09 15:44:26 -0500

    Modified offering attributes

--- Application/src/DB/Offering.java
+++ Application/src/DB/Offering.java
@@ -3,24 +3,26 @@ package DB; // Adjust the package as needed
 public class Offering {
     private String title;
     private String organization;
-    private String instructor;
-    private String location;
+    private String city;
     private String time;
     private int capacity;
+    private int numStudents;
+    private Integer instructorId; //using Integer wrapper class because instructorId can be null
 
-    public Offering(String title, String organization, String instructor, String location, String time, int capacity) {
+    public Offering(String title, String organization, String city, String time, int capacity) {
         this.title = title;
         this.organization = organization;
-        this.instructor = instructor;
-        this.location = location;
+        this.city = city;
         this.time = time;
         this.capacity = capacity;
+        this.numStudents = 0;
+        this.instructorId = null;
     }
 
     public String getTitle() { return title; }
     public String getOrganization() { return organization; }
-    public String getInstructor() { return instructor; }
-    public String getLocation() { return location; }
+    public int getInstructorId() { return instructorId; }
+    public String getCity() { return city; }
     public String getTime() { return time; }
     public int getCapacity() { return capacity; }
 }
